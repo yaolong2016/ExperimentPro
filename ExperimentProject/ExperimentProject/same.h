bb175a6d94d4c4bfcf0a6513519b5aafe8b68802
YJM <490964242@qq.com>
2016-11-10 10:59:20 +0800

    hgh

---
+++ ExperimentProject/ExperimentProject/same.h
@@ -0,0 +1,13 @@
+//
+//  same.h
+//  ExperimentProject
+//
+//  Created by YJM on 2016/11/10.
+//  Copyright © 2016年 yaolong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface same : NSObject
+
+@end
---
+++ ExperimentProject/ExperimentProject/same.m
@@ -0,0 +1,13 @@
+//
+//  same.m
+//  ExperimentProject
+//
+//  Created by YJM on 2016/11/10.
+//  Copyright © 2016年 yaolong. All rights reserved.
+//
+
+#import "same.h"
+
+@implementation same
+
+@end
