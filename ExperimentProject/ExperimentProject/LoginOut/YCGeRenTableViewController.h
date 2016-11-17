10aaf4be11aeb2788183dd5b4a1b4e98cff8a5e3
YJM <490964242@qq.com>
2016-11-17 14:15:52 +0800

    个人详情的创建

---
+++ ExperimentProject/ExperimentProject/LoginOut/YCGeRenTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  YCGeRenTableViewController.h
+//  ExperimentProject
+//
+//  Created by YJM on 2016/11/17.
+//  Copyright © 2016年 yaolong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YCGeRenTableViewController : UITableViewController
+
+@end
